aecaca47c6d07f387da2bd0c4de8f677816aade7
Crystal-CodeMaster <felipehenriquecrispim@gmail.com>
2025-04-10 09:40:27 +0900

    Added a third-branch
    Co-authored-by: CodeMaster0099 <felipehenriquecrispim@gmail.com>

--- server/vite.ts
+++ server/vite.ts
@@ -8,7 +8,6 @@ const __dirname = dirname(__filename);
 import { type Server } from "http";
 import viteConfig from "../vite.config";
 import { nanoid } from "nanoid";
-
 const viteLogger = createLogger();
 
 export function log(message: string, source = "express") {
